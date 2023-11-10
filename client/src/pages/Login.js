affa409e37e9c6c49b7ee84e81bf28ffe63fd822
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-11-10 13:54:55 -0300

    register & login routing form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,50 @@
 import React from "react";
-
+import { Form, Input, message } from "antd";
+import "../styles/LoginStyles.css";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 const Login = () => {
+  const navigate = useNavigate();
+  //form handler
+  const onFinishHandler = async (values) => {
+    try {
+      const res = await axios.post("/api/v1/user/login", values);
+      if (res.data.success) {
+        localStorage.setItem("token", res.data.token);
+        message.success("Login succesfully");
+        navigate("/");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      message.error("something went wrong");
+    }
+  };
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <div className="form-container">
+        <Form
+          layout="vertical"
+          onFinish={onFinishHandler}
+          className="register-form"
+        >
+          <h3 className="text-center">Login Form</h3>
+          <Form.Item label="Email" name="email">
+            <Input type="text" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="text" required />
+          </Form.Item>
+          <Link to="/register" className="m-2">
+            I don't have an account
+          </Link>
+          <button className="btn btn-primary" type="submit">
+            Login
+          </button>
+        </Form>
+      </div>
+    </>
   );
 };
 
