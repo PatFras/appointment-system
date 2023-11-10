f5efc820f1300c36dc32c313b1e7ea9645850d38
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-11-10 14:13:40 -0300

    db connection + user model + routes + controller

--- server.js
+++ server.js
@@ -2,20 +2,19 @@ const express = require("express");
 const colors = require("colors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 //dotenv config
 dotenv.config();
+//mongoDB connection
+connectDB();
 //rest object
 const app = express();
 //middlewares
 app.use(express.json());
 app.use(morgan("dev"));
 //routes
-app.get("/", (req, res) => {
-  res.status(200).send({
-    message: "Server running",
-  });
-});
+app.use("/api/v1/user", require("./routes/userRoutes"));
 //port
 const port = process.env.PORT || 8080;
 //listen port
